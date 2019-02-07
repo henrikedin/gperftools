xiexiaoy/eloqdoc

SERVER-17788 [GPerfTools-2.2 Patch] improve tcmalloc freelist stats
    Walks the size classes in TCMalloc's central freelist, exposing interesting
    statistics via a callback.

    serverStatus() now includes this information in lieu of the TCMalloc dump stats
    text block.

src/third_party/gperftools-2.5/src/tcmalloc.cc
@@ -912,6 +912,31 @@ class TCMallocImplementation : public MallocExtension {
       v->push_back(i);
     }
   }
+  
+  virtual void SizeClasses(void* arg, SizeClassFunction func) {
+    TCMallocStats global_stats;
+    base::MallocSizeClass stats;
+    uint64_t class_count[kNumClasses];
+    ExtractStats(&global_stats, class_count, NULL, NULL);
+
+    for (int cl = 0; cl < kNumClasses; cl++) {
+      uint64_t central_objs = Static::central_cache()[cl].length();
+      uint64_t transfer_objs = Static::central_cache()[cl].tc_length();
+      uint64_t num_spans = Static::central_cache()[cl].spans();
+      uint64_t pages_per_span = Static::sizemap()->class_to_pages(cl);
+
+      stats.bytes_per_obj = Static::sizemap()->ByteSizeForClass(cl);
+      stats.pages_per_span = pages_per_span;
+      stats.num_spans = num_spans;
+      stats.num_central_objs = central_objs;
+      stats.num_transfer_objs = transfer_objs;
+      stats.num_thread_objs = class_count[cl] - central_objs - transfer_objs;
+      stats.free_bytes = class_count[cl] * Static::sizemap()->ByteSizeForClass(cl);
+      stats.alloc_bytes = (num_spans * pages_per_span) << kPageShift;
+
+      func(arg, &stats);
+    }
+  }
 };
 
 // The constructor allocates an object to ensure that initialization

henrikedin/gperftools

src/tcmalloc.cc
@@ -1006,6 +1006,31 @@ class TCMallocImplementation : public MallocExtension {
       v->push_back(i);
     }
   }
+  
+  virtual void SizeClasses(void* arg, SizeClassFunction func) {
+    TCMallocStats global_stats;
+    base::MallocSizeClass stats;
+    uint64_t class_count[kClassSizesMax];
+    ExtractStats(&global_stats, class_count, NULL, NULL);
+
+    for (int cl = 0; cl < Static::num_size_classes(); cl++) {
+      uint64_t central_objs = Static::central_cache()[cl].length();
+      uint64_t transfer_objs = Static::central_cache()[cl].tc_length();
+      uint64_t num_spans = Static::central_cache()[cl].spans();
+      uint64_t pages_per_span = Static::sizemap()->class_to_pages(cl);
+
+      stats.bytes_per_obj = Static::sizemap()->ByteSizeForClass(cl);
+      stats.pages_per_span = pages_per_span;
+      stats.num_spans = num_spans;
+      stats.num_central_objs = central_objs;
+      stats.num_transfer_objs = transfer_objs;
+      stats.num_thread_objs = class_count[cl] - central_objs - transfer_objs;
+      stats.free_bytes = class_count[cl] * Static::sizemap()->ByteSizeForClass(cl);
+      stats.alloc_bytes = (num_spans * pages_per_span) << kPageShift;
+
+      func(arg, &stats);
+    }
+  }
 };
 
 static inline ATTRIBUTE_ALWAYS_INLINE